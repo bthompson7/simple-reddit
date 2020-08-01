2ec60c2a8d926af8ff02ef0df97f568a62bdaa46
Ben Thompson <benjamin.philip.thompson@maine.edu>
2020-07-31 22:32:56 -0400

    backend stuff

--- backend/server.py
+++ backend/server.py
@@ -5,6 +5,8 @@ main backend for reddit rewritten
 handles:
 1. login/register of users
 
+Backend TODO:
+1. setup login/register tables
 
 '''
 
@@ -17,6 +19,7 @@ from twisted.web.server import Site
 from twisted.web.wsgi import WSGIResource
 from flask_cors import CORS
 import json
+from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
 CORS(app)
@@ -27,22 +30,41 @@ def page_not_found(e):
 
 @app.route('/login',methods=['POST'])
 def login():
-    pass
+    #if username exists and password we are given matches the stored password we are good
+    data = request.json
+    username = data["name"]
+    password = data["pw"]
+    '''
+    essentially to check the password just hash the password
+     we are given and if it matches the one in the db we are good
+
+    https://pythonprogramming.net/password-hashing-flask-tutorial/
+    encryptPassword = sha256_crypt.encrypt(password)
+    print(encryptPassword)
+		
+    '''
+    return jsonify('code: 200'),200
+
 
 @app.route('/register',methods=['POST'])
 def register():
     data = request.json
     username = data["name"]
     password = data["pw"]
+    #if username doesn't exist then we are good
+
+    '''
+    https://pythonprogramming.net/password-hashing-flask-tutorial/
+    encryptPassword = sha256_crypt.encrypt(password)
+    print(encryptPassword)	
+    '''
     return jsonify('code: 200'),200
 
 @app.route('/api/upvote',methods=['POST'])
-@auth.required
 def upvote():
     pass
 
 @app.route('/api/downvote',methods=['POST'])
-@auth.required
 def downvote():
     pass
 
@@ -53,7 +75,10 @@ def get_posts():
 
 #connect to database
 def db_con():
-    pass
+    global cursor
+    global db
+    db = pymysql.connect("localhost","monitor","password","temps")
+    cursor = db.cursor()
 
 resource = WSGIResource(reactor, reactor.getThreadPool(), app)
 site = Site(resource)
